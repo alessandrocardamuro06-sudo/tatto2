e420f77617b386b94b4f386cf36dd0a251e36667
alessandrocardamuro06-sudo <alessandrocardamuro06@gmail.com>
2026-05-01 13:54:59 +0200

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7hlQvABDBpZiz27MROAD5Uar-YqgZC7s",
+  authDomain: "tattostudio-871a8.firebaseapp.com",
+  projectId: "tattostudio-871a8",
+  storageBucket: "tattostudio-871a8.firebasestorage.app",
+  messagingSenderId: "356880787394",
+  appId: "1:356880787394:web:1f20922496f5e8db9c985b"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
