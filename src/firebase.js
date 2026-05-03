5b452d6003332e826e0f34e3c1ad406a6638aa41
alessandrocardamuro06-sudo <alessandrocardamuro06@gmail.com>
2026-05-03 15:25:12 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB7hlQvABDBpZiz27MROAD5Uar-YqgZC7s",
@@ -17,4 +16,3 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app);
